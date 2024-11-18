2cf2e983be716bc2163a6a1e8725cfe2fb049e90
Kapil Bhattarai <kapilbhattarai502@gmail.com>
2024-11-18 22:39:26 +0545

    feat:added dropdown avatar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,51 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { useNavigate } from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
-import { Button } from "antd";
+import { Button, Typography } from "antd";
+import {
+  Avatar,
+  Box,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Tooltip,
+
+} from "@mui/material";
+import { Logout, PersonAdd, Settings } from "@mui/icons-material";
+
 
 const Header = () => {
   const navigate = useNavigate();
 
-  
-  const [isuser,setisUser]=useState<string|null>(()=>{
-    return localStorage.getItem('currentUser');
-  })
-  
-  
- 
+  const [isuser, setisUser] = useState<string | null>(() => {
+    return localStorage.getItem("currentUser");
+  });
+  const [data, setData] = useState([]);
 
-
-  const handleLogout = () => {
-    if (window.confirm("Are you sure you want to logout?")) {
-
-        localStorage.removeItem("role");
-        localStorage.removeItem("token");
-        localStorage.removeItem("currentUser");
-        setisUser(null);
-        navigate("/");
-     
-    }
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
-
+  const handleLogout = () => {
+    
+      localStorage.removeItem("role");
+      localStorage.removeItem("token");
+      localStorage.removeItem("currentUser");
+      setisUser(null);
+      navigate("/");
+    
+  };
 
   return (
     <>
@@ -48,7 +62,7 @@ const Header = () => {
             Vastra
           </h1>
         </div>
-        <div className="flex gap-3 cursor-pointer">
+        <div className="flex gap-3 cursor-pointer items-center">
           <div className="">
             <Stack spacing={2} sx={{ width: 300 }}>
               <Autocomplete
@@ -93,27 +107,107 @@ const Header = () => {
               sx={{ fontSize: "2rem", color: "#333" }}
               onClick={() => navigate("/clothes/cart")}
             />
-            {/* {isAuthenticated && (
-              <div className="h-5 w-5 rounded-full bg-orange-500 absolute -top-2 right-0 border border-white left-4">
-                <p className="text-white font-bold text-sm text-center">
-                  {cartItem?.length}
-                </p>
-              </div>
-            )} */}
           </div>
           {isuser ? (
-            // <LogoutIcon
-            //   sx={{ fontSize: "2rem", color: "#333" }}
-            //   onClick={handleLogout}
-            // />
-            <Button type="primary" onClick={handleLogout} danger className="py-2">LogOut</Button>
-
+            <>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  textAlign: "center",
+                }}
+              >
+                <Tooltip title="Account settings">
+                  <IconButton
+                    
+                    onClick={handleClick}
+                    size="small"
+                    aria-controls={open ? "account-menu" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? "true" : undefined}
+                  >
+                    <Avatar sx={{ width: 32, height: 32,backgroundColor:"#333" }}>{isuser?.split("")[0]}</Avatar>
+                  </IconButton>
+                </Tooltip>
+              </Box>
+              <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                slotProps={{
+                  paper: {
+                    elevation: 0,
+                    sx: {
+                      overflow: "visible",
+                      filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                      mt: 1.5,
+                      "& .MuiAvatar-root": {
+                        width: 32,
+                        height: 32,
+                        ml: -0.5,
+                        mr: 1,
+                      },
+                      "&::before": {
+                        content: '""',
+                        display: "block",
+                        position: "absolute",
+                        top: 0,
+                        right: 14,
+                        width: 10,
+                        height: 10,
+                        bgcolor: "background.paper",
+                        transform: "translateY(-50%) rotate(45deg)",
+                        zIndex: 0,
+                      },
+                    },
+                  },
+                }}
+                transformOrigin={{ horizontal: "right", vertical: "top" }}
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+              >
+                <MenuItem onClick={handleClose}>
+                  <Avatar /> Profile
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <Avatar /> My account
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <PersonAdd fontSize="small" />
+                  </ListItemIcon>
+                  Add another account
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <Settings fontSize="small" />
+                  </ListItemIcon>
+                  Settings
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <Logout fontSize="small" />
+                  </ListItemIcon>
+                  <div onClick={handleLogout}>
+                  Logout
+                  </div>
+                </MenuItem>
+              </Menu>
+            </>
           ) : (
             // <LoginIcon
             //   sx={{ fontSize: "2rem", color: "#333" }}
             //   onClick={() => navigate("/login")}
             // />
-            <Button type="primary" onClick={() => navigate("/login")} className="py-2">LogIn</Button>
+            <Typography
+              
+              onClick={() => navigate("/login")}
+              className="py-2 text-lg font-semibold"
+            >
+              LogIn
+            </Typography>
           )}
         </div>
       </div>
